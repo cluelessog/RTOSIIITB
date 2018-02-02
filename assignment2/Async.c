c4d6a977a17223527724952ec7dd42b85bce6cd6
sourabh kumar <sourabhkumar0308@gmail.com>
2018-02-02 19:05:42 +0530

    Async and Push added

---
+++ assignment2/Async.c
@@ -0,0 +1,35 @@
+#include <errno.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <string.h>
+// defining GPIO pins
+
+#define GPIO_1  0  //physical pin 11
+#define	GPIO_2	1  //physical PIN 12
+#define	GPIO_3	2  //physical PIN 13
+#define	GPIO_4	3  //physical PIN 15
+
+
+
+int main (void)
+{
+  wiringPiSetup () ;
+  //configuring pins in input mode
+  pinMode (GPIO_1, INPUT) ;
+  pinMode (GPIO_2, INPUT) ;
+  pinMode (GPIO_3, INPUT) ;
+  pinMode (GPIO_4, INPUT) ;
+  //declaring variable to store pin status
+  int gpio_1,gpio_2,gpio_3,gpio_4;
+  //assigning status
+  gpio_1 = digitalRead(GPIO_1);
+  gpio_2 = digitalRead(GPIO_2);
+  gpio_3 = digitalRead(GPIO_3);
+  gpio_4 = digitalRead(GPIO_4);
+  printf("Received Value is : %d\n",(8*gpio_4 + 4*gpio_3 + 2*gpio_2 + gpio_1)); 
+  return 0 ;
+}
+/* References
+ * http://www.science.smith.edu/dftwiki/index.php/Tutorial:_Interrupt-Driven_Event-Counter_on_the_Raspberry_Pi
+ * https://github.com/ngs/wiringPi/blob/master/examples/isr.c
+ */
